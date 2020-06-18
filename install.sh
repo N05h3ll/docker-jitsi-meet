9aafc4c0e4b7516f4764cdf90887858eb06c7b56
Mohamed Ashraf <37964825+N05h3ll@users.noreply.github.com>
2020-06-18 02:45:13 +0200

    add auto install scrip.

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp env.example .env
+./gen-passwords.sh
+mkdir -p ~/.jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
+docker-compose up -d
