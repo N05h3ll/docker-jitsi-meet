b0e75b1a14be9d8ee362baf7d416935d10e95218
N05h3ll <m.ashm985@gmail.com>
2020-06-18 02:48:57 +0200

    fix install issues

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp env.example .env
-./gen-passwords.sh
-mkdir -p ~/.jitsi-meet-cfg/{web/letsencrypt,transcripts,prosody/config,prosody/prosody-plugins-custom,jicofo,jvb,jigasi,jibri}
-docker-compose up -d
